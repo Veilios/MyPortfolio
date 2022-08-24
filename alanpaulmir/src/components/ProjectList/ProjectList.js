6fe90ecc6b6a3f3ba2d7d9ee24b2578b6e5e2531
Alan Mir <alanpaulphotos@gmail.com>
2022-08-24 17:48:57 -0400

    Added motion animation to project page

--- alanpaulmir/src/components/ProjectList/ProjectList.js
+++ alanpaulmir/src/components/ProjectList/ProjectList.js
@@ -1,17 +1,21 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
 import ProjectCard from '../ProjectCard/ProjectCard.js';
 import { products } from "../../data";
+
 import "./ProjectList.scss";
 
 const ProjectList = () => {
     return (
         <div className="pl" >
-            <div className="pl-texts" >
+            <motion.div className="pl-texts" whileInView={{opacity: 1}} transition={{ duration: 0.85, ease: 'easeOut', delay: 0.5}} initial={{ opacity: 0 }}>
                 <h1 className="pl-title" >Create & Inspire.</h1>
                 <p className="pl-desc" >
                     Of the years I've been a a Software Developer, These are some of my
                     favorite projects to date. Take a look around!
                 </p>
-            </div>
+            </motion.div>
             <div className="pl-list" >
                 {products.map((item) => (
                     <ProjectCard key={item.id} img={item.img} link={item.link} title={item.title}/>
