fd2c2958562999b040c19a881fec46b26f6814bc
Alan Mir <alanpaulphotos@gmail.com>
2022-08-26 21:47:58 -0400

    Changed resume link, and animated the navbar

--- alanpaulmir/src/components/Nav/Navbar.js
+++ alanpaulmir/src/components/Nav/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <div className='nav-container'>
-      <nav>
+      <motion.nav animate={{x: [-300, 0]}} transition={{duration: 0.85, ease: 'easeOut'}}>
         <a href='#home'>Home</a>
         <a href='#bio'>About</a>
         <a href='#projects'>Projects</a>
         <a href='#contact'>Contact</a>
-      </nav>
+      </motion.nav>
 
-      <nav>
+      <motion.nav animate={{x: [300, 0]}} transition={{duration: 0.85, ease: 'easeOut'}}>
         <a href='#1'>Blog</a>
-        <a rel='noreferrer' target="_blank" href='https://www.linkedin.com/ambry/?x-li-ambry-ep=AQKsXsuYHfsOcQAAAYLSZeSM4YGqtHfOmLvpe20q-8bz1d6irmtSvTsEjuZy8s1TQbPpKIxIlOD2rHvQbhMI33wiEholbsVLFq-cF64pBk4QjQyyBtC0w9wSHy5s1pYXHiBZ8PWWIP9971cXbReKtvi6ETDLdy1oULN7FuKsPK4kSlwscGQRFn0Loj1EHi4tZDaQg-etIigMLNwxmlhPf7llLbJzDnuv0Zb4SvbOTKEBlBu3t-yhCw8_NUvrisJPq8JaDgxUhLn_MkB13ZFNJsmYJ08L-ul2LRL8nt6-BPPN59-F2hKxu6liEdqu6tvGqJ6TOwHspfqr-2gQgsIjhHRRmlh83yGa7n42AZvuIoaoj9kTdRDMEcK6uTGQ7gciuweOWwik4ROl3xEnupHwveFM-x2c0FAg_pRgp99qRsDJPxtuB_VlCqWrhv2bv5Hn-B_seFGnyTWTPN-yTerPJfGuCCLC08jpAI6coJ5zUdTKeJraUXySEVR7jB4mkH65WKNCwmmr7XVASW7g6RDakwtXwCF1fRJZ8gQFVOZtG23ATdeoykXu8MSsdyXVoaEj5xIh' download>Resume</a>
-      </nav>
+        <a rel='noreferrer' target="_blank" href='https://drive.google.com/file/d/1RwdVqVd4d0d7WNqqpdQN85h-lxh2qrBD/view?usp=sharing' download>Resume</a>
+      </motion.nav>
     </div>
   );
 };
